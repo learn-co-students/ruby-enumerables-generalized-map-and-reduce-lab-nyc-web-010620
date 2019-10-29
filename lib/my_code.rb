24baaa034ece5b64e8f2d56dd8e3bded8f44cc7d
Jerry Huang <jhuang9240@gmail.com>
2019-10-29 16:07:53 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+def map(array)
+  i = 0
+  result = []
+  while i < array.length do
+    result.push(yield(array[i]))
+    i += 1
+  end
+  result
+end
+
+
+def reduce(array,start_point = 0)
+  i = 0
+  result = start_point
+
+  while i < array.length do
+    if array[i].is_a?! Integer
+      
+  end
+  
+  
+  while i < array.length do
+    result = yield(result, array[i])
+    i += 1
+  end
+    result
+  end
