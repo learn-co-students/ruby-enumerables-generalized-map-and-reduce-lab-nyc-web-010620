2e1fde2ab0e0718c344b31f3e9f9af2f4d19d4d1
Miles Higbie <miles.higbie@gmail.com>
2019-11-29 19:55:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,18 @@ def map(array)
     end 
   return new_arr
 end 
+
+def reduce(array, sv = nil)
+  if sv 
+    sum = sv 
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1
+  end
+    while i < array.length do 
+      sum  = yield(sum, array[i])
+    i += 1
+    end 
+  return sum
+end 
